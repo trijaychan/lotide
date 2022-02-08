31dd9b136b80c6afa43e04920d0b6eb3e956520e
trijaychan <your@email.com>
2022-02-08 00:31:21 +0000

    used string interpolation

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅ Assertion Failed: ${actual} !== ${expected}`);
   } else {
-    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
