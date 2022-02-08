3967d17fb573e7232a5094772752d0714c6d7069
trijaychan <your@email.com>
2022-02-08 19:51:49 +0000

    working

--- head.js
+++ head.js
@@ -8,7 +8,7 @@ const assertEqual = function(actual, expected) {
 
 const head = function(arr) {
   return arr[0];
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
