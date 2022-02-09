5186dc7d923c4fbb1e6f3be8709e90505af843af
trijaychan <trijaychan@gmail.com>
2022-02-09 19:18:20 +0000

    working

---
+++ countOnly.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let count = {};
+
+  for (let key in itemsToCount) {
+    if (itemsToCount[key] === true) {
+      for (let item of allItems) {
+        if (item === key) {
+          count[key] === undefined ? count[key] = 1 : count[key]++;
+        }
+      }
+    }
+  }
+  return count;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
