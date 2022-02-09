039d22584e6a44c82ef09f6b57867e9883d6cfee
trijaychan <trijaychan@gmail.com>
2022-02-09 19:48:05 +0000

    working

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      results[sentence[i].toLowerCase()] === undefined ? results[sentence[i].toLowerCase()] = [i] : results[sentence[i].toLowerCase()].push(i);
+    }
+  }
+
+  return results;
+};
+
+let positions = letterPositions("lighthouse in the house");
+
+console.log(positions);
+
